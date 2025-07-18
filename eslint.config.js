da29bfeb2eb589cf6f7d40b5fe11324b64c4cf6b
MadiniMoyoni <info@madinimoyoni.co.uk>
2025-07-18 02:44:03 +0100

    chore: relax lint rules and ignore dist/

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,10 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // --- temporary relaxations while scaffolding ---
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-case-declarations': 'warn',
     },
-  }
+  },
 );
